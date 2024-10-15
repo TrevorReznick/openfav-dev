da7c89a96dd8a6e7650f32a957ceee1355a16a24
TrevorReznick <135354265+TrevorReznick@users.noreply.github.com>
2024-10-15 13:17:23 +0200

    OPF-18 created type postData

---
+++ src/types/postData.ts
@@ -0,0 +1,27 @@
+export type postData = {
+    accessible: boolean
+    AI: boolean
+    AI_Summary?: string
+    AI_think?: string
+    description: string
+    domain_exists: boolean
+    html_content_exists: boolean
+    icon?: string
+    id_cat?: number
+    id_provider?: number
+    image?: string
+    is_public: boolean
+    logo?: string
+    name: string
+    ratings?: number
+    secure: boolean
+    status_code: number
+    tag_3?: number
+    tag_4?: number
+    tag_5?: number
+    title: string
+    type: string
+    url: string
+    user_id: string
+    valid_url: boolean
+}
