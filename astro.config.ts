c54b9641b8725743b0eb3c4e17d212d33b2a8d59
Tuo Nome <double.facessss@gmail.com>
2024-10-26 11:11:36 +0200

    OPF-31 creating Services section

--- astro.config.ts
+++ astro.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         tabler: ['*'],
         'flat-color-icons': [
           'template',
+          'bookmark',
           'gallery',
           'approval',
           'document',
