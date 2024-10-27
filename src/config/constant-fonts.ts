b0bfcc26876ed17bdbbc4d1e79a7daae29935ab7
Trevor Reznick <electrics.sheeps@gmail.com>
2024-10-27 19:49:18 +0100

    OPF-31 settings fonts costants

--- src/config/constant-fonts.ts
+++ src/config/constant-fonts.ts
@@ -1,8 +1,13 @@
 export const H1_HERO = 'text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide text-primaryText px-8 sm:px-8 md:px-20 lg:px-4'
+export const H1_HERO_SUB = 'mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide text-primaryText px-8 sm:px-20 md:px-24 lg:px-24'
 export const H2_SUBTITLE = 'text-secondaryText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48'
-export const P_TAGLINE = 'text-secondaryColor text-sm sm:text-base mb-6 sm:mt-32 mt-16 font-bold'                            
+export const P_TAGLINE = 'text-secondaryColor text-sm sm:text-base mb-6 sm:mt-32 mt-16 font-bold'                           
 export const FAQ_TITLE = 'mb-7 block-subtitle text-center'
 export const FAQ_HEADLINE = 'mb-7 block-subtitle text-center'
 export const FAQ_H2_TITLE = 'mb-16 block-big-title text-center'
 export const FAQ_H3_SUBTITLE = 'content-title pt-3 sm:pt-0 pr-8 sm:pr-0'
+export const SECTION_TITLE = 'mt-6 mb-8 text-4xl lg:text-5xl block-big-title'
+export const SECTION_SUBITITLE = 'mb-10 text-secondaryText leading-loose'
+export const SECTION_LIST_WHITE = 'mb-6 text-primaryText'
+
 
