1f352520e5837e1a044a101008068d40a9ceb9c9
TrevorReznick <135354265+TrevorReznick@users.noreply.github.com>
2024-11-17 08:46:48 +0100

    OPF-49 debugging 4

--- src/pages/api/v1/auth/signin.ts
+++ src/pages/api/v1/auth/signin.ts
@@ -33,7 +33,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   if (!email || !password) {
     //return new Response('Email and password are required', { status: 400 })
     //return redirect('/auth-error-page')
-    return redirect('/error-email-password')
+    return redirect('/email-password-error')
   }
 
   const { data, error } = await supabase.auth.signInWithPassword({
