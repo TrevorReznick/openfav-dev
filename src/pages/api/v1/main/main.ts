63c027f8aab7c0fbb01c9e6237fd44e7113e08c8
Vincent Maximo II <projects.manager@xdomain.it>
2024-12-01 16:59:39 +0100

    OPF-59 created new enevents api file

--- src/pages/api/v1/main/main.ts
+++ src/pages/api/v1/main/main.ts
@@ -1,12 +1,9 @@
-import type { APIRoute } from 'astro'
-import { supabase } from '../../../../providers/supabase'
-import type { postData } from '../../../../types/postData'
-
+import type { APIRoute } from "astro";
+import { supabase } from "~/providers/supabase";
+import type { postData } from "~/types/postData";
 
 export const GET: APIRoute = async () => {
-  
-  const { data, error } = await supabase
-    .from('main_table').select(`
+  const { data, error } = await supabase.from("main_table").select(`
       id,
       description,
       icon,
@@ -38,151 +35,145 @@ export const GET: APIRoute = async () => {
         type,
         AI
       )
-    `)   
-    //.order("id", { ascending: true });
-    
+    `);
+  //.order("id", { ascending: true });
+
   if (error) {
     return new Response(
       JSON.stringify({
         error: error.message,
       }),
-      { status: 500 },
+      { status: 500 }
     );
   }
 
-  return new Response(JSON.stringify(data))
-}
+  return new Response(JSON.stringify(data));
+};
 export const POST: APIRoute = async ({ request }) => {
-  
-    const {
+  const {
+    /* @@ main_table @@ */
 
-        /* @@ main_table @@ */
+    description,
+    icon,
+    image,
+    logo,
+    name,
+    title,
+    url,
 
-        description,
-        icon,
-        image,
-        logo,
-        name,
-        title,
-        url,
+    /* @@ sub_main_table @@ */
 
-        /* @@ sub_main_table @@ */
-        
-        user_id,
-        accessible,
-        domain_exists,
-        html_content_exists,
-        is_public,
-        secure, 
-        status_code,
-        valid_url,
-        type,
-        AI,
-        
+    user_id,
+    accessible,
+    domain_exists,
+    html_content_exists,
+    is_public,
+    secure,
+    status_code,
+    valid_url,
+    type,
+    AI,
 
-        /* @ categories_tag @*/
+    /* @ categories_tag @*/
 
-        id_area,//
-        id_cat,//
-        id_provider,//               
-        ratings,        
-        tag_3, //
-        tag_4, 
-        tag_5,
-        AI_Summary,
-        AI_think,
-        
-        /* @@ common @@ */
+    id_area, //
+    id_cat, //
+    id_provider, //
+    ratings,
+    tag_3, //
+    tag_4,
+    tag_5,
+    AI_Summary,
+    AI_think,
 
-        id_src
-    }: postData = await request.json()
+    /* @@ common @@ */
 
-    /* @@ post main_table @@ */
+    id_src,
+  }: postData = await request.json();
 
-    const { data: _data, error: _error } = await supabase
-        .from('main_table')
-        .insert({
-            description,
-            icon,
-            image,
-            logo,
-            name,
-            title,
-            url
-        })
-        .select()
+  /* @@ post main_table @@ */
 
-    if (_error) {
-        return new Response(
-        JSON.stringify({
-            error: _error.message,
-        }),
-        { status: 500 },
-        )
-    }
+  const { data: _data, error: _error } = await supabase
+    .from("main_table")
+    .insert({
+      description,
+      icon,
+      image,
+      logo,
+      name,
+      title,
+      url,
+    })
+    .select();
 
-    const _id_src = _data ? _data[0].id : -1  
+  if (_error) {
+    return new Response(
+      JSON.stringify({
+        error: _error.message,
+      }),
+      { status: 500 }
+    );
+  }
 
-    const payload_sub_main_table = {
-        id_src: _id_src,
-        user_id,
-        accessible: accessible ? true : false,
-        AI: AI ? true : false,
-        domain_exists:  domain_exists ? true : false,
-        html_content_exists:  html_content_exists ? true : false,
-        is_public: true,
-        type,
-        valid_url: valid_url ? true : false,
-        status_code,
-        secure: secure ? true : false         
-    }
+  const _id_src = _data ? _data[0].id : -1;
 
-    const { data: __data, error: __error } = await supabase
+  const payload_sub_main_table = {
+    id_src: _id_src,
+    user_id,
+    accessible: accessible ? true : false,
+    AI: AI ? true : false,
+    domain_exists: domain_exists ? true : false,
+    html_content_exists: html_content_exists ? true : false,
+    is_public: true,
+    type,
+    valid_url: valid_url ? true : false,
+    status_code,
+    secure: secure ? true : false,
+  };
 
-        .from('sub_main_table')
-        .insert(payload_sub_main_table)  
-        .select()
+  const { data: __data, error: __error } = await supabase
 
-    if (__error) {
-      return new Response(
-        JSON.stringify({
-          error: __error.message,
-        }),
-        { status: 501 },
-      )
-    }
-  const msg = 'inserimento avvenuto con successo'
+    .from("sub_main_table")
+    .insert(payload_sub_main_table)
+    .select();
 
-  
+  if (__error) {
+    return new Response(
+      JSON.stringify({
+        error: __error.message,
+      }),
+      { status: 501 }
+    );
+  }
+  const msg = "inserimento avvenuto con successo";
 
   const payload = {
     id_src: _id_src,
     id_area: id_area ? id_area : -1,
     id_cat: id_cat ? id_cat : -1,
-    tag_3: tag_3 ? tag_3 : -1,    
+    tag_3: tag_3 ? tag_3 : -1,
     tag_4: tag_4 ? tag_4 : -1,
     tag_5: tag_5 ? tag_5 : -1,
     id_provider,
     ratings,
     AI_think,
-    AI_Summary
-  }
+    AI_Summary,
+  };
 
   const { data: ___data, error: ___error } = await supabase
 
-    .from('categories_tags')
-    .insert(payload)  
-    .select()  
+    .from("categories_tags")
+    .insert(payload)
+    .select();
 
   if (___error) {
     return new Response(
       JSON.stringify({
         error: ___error.message,
       }),
-      { status: 501 },
-    )
+      { status: 501 }
+    );
   }
 
-  return new Response(JSON.stringify({'msg': msg}), { status: 200 })
-
-}
+  return new Response(JSON.stringify({ msg: msg }), { status: 200 });
+};
