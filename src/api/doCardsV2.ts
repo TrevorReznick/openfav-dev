fe55f4b6fee4312a79b8e104736139e77d286d44
Vincent Maximo II <projects.manager@xdomain.it>
2024-12-01 19:37:57 +0100

    OPF-58 staging

--- src/api/doCardsV2.ts
+++ src/api/doCardsV2.ts
@@ -1,61 +1,84 @@
-import {initCard} from '~/scripts/cardClass'
-import type { ListType, MyIconType, ActionType, ActivityItem } from '~/types'
+import { initCard } from "~/scripts/cardClass";
+import type { ListType, MyIconType, ActionType, ActivityItem } from "~/types";
+import { getEvents } from "~/scripts/requests";
 
 // type: 'urls' | 'lists', name: string
 const createCardWithActivities = (type, name) => {
-  const card = initCard(type, name)
+  const card = initCard(type, name);
   return {
-    getCardData: () => card().getCardData(),    
-    updateCardTitle: (title) => card().updateCardTitle(title),    
-    updateCard: (updates) => card().updateCard(updates),    
-    addActivity: (action, actionIcon, details) => 
-      card().addActivity(action, actionIcon, details)
-  }
-}
+    getCardData: () => card().getCardData(),
+    updateCardTitle: (title) => card().updateCardTitle(title),
+    updateCard: (updates) => card().updateCard(updates),
+    addActivity: (action, actionIcon, details) =>
+      card().addActivity(action, actionIcon, details),
+  };
+};
 
-// Creazione delle card
-const urlsCard = createCardWithActivities('urls', 'my cardname')
-const listsCard = createCardWithActivities('lists', 'another cardname')
+/* get @@ remote data @@ */
 
-// Aggiornamento e aggiunta di attività alle card
-urlsCard.updateCardTitle('Nuovo Titolo')
+const my_events: any = await getEvents();
+
+/* @@ Creazione delle card @@ */
+
+const urlsCard = createCardWithActivities("urls", "my cardname");
+
+urlsCard.updateCardTitle("Last Insertions");
 
 urlsCard.updateCard({
-  cardIcon: 'link',
-  action_url: 'https://stinkyfeet.com'
-})
-urlsCard.addActivity('Created List', 'folder', {
-  description: 'Card creata da Vincenzo Navarra',
-  name: 'Enzo'
-})
-urlsCard.addActivity('Created List', 'clock', {
-  description: 'Card creata da Luigi Frisco',
-  name: 'Enzo'
-})
-listsCard.updateCardTitle('Nuovo Titolo 1')
+  cardIcon: "link",
+  action_url: "https://example.com",
+});
 
-listsCard.addActivity('Updated List', 'clock', {
-  description: 'Lista aggiornata da Dino di Piazza',
-  name: 'Maria'
-})
-listsCard.addActivity('Updated List', 'link', {
-  description: 'Lista aggiornata da Dino di Piazza',
-  name: 'Enzo'
-})
+const urlsEvents = my_events.filter((event) => {
+  event.event_type.event_type === "urls";
+  urlsCard.addActivity("Created List", "folder", {
+    description: "Card creata da Vincenzo Navarra",
+    name: "Enzo",
+  });
+});
+
+console.log("good night", urlsEvents.event_type);
+
+const listsCard = createCardWithActivities("lists", "another cardname");
+
+// Aggiornamento e aggiunta di attività alle card
+urlsCard.updateCardTitle("Nuovo Titolo");
+
+urlsCard.updateCard({
+  cardIcon: "link",
+  action_url: "https://example.com",
+});
+urlsCard.addActivity("Created List", "folder", {
+  description: "Card creata da Vincenzo Navarra",
+  name: "Enzo",
+});
+urlsCard.addActivity("Created List", "clock", {
+  description: "Card creata da Luigi Frisco",
+  name: "Enzo",
+});
+listsCard.updateCardTitle("Nuovo Titolo 1");
+
+listsCard.addActivity("Updated List", "clock", {
+  description: "Lista aggiornata da Dino di Piazza",
+  name: "Maria",
+});
+listsCard.addActivity("Updated List", "link", {
+  description: "Lista aggiornata da Dino di Piazza",
+  name: "Enzo",
+});
 listsCard.updateCard({
-  cardIcon: 'folderHeart',
-  action_url: 'https://example.com'
-})
+  cardIcon: "folderHeart",
+  action_url: "https://example.com",
+});
 
 // Oggetto featuredCard contenente i dati delle card
 // Struttura: { urlsCard: CardData, listsCard: CardData }
 export const featuredCards = {
   urlsCard: urlsCard.getCardData(),
-  listsCard: listsCard.getCardData()
-}
+  listsCard: listsCard.getCardData(),
+};
 
 // Uso:
 // import { featuredCard } from './scripts/myCards'
 // const urlsCardData = featuredCard.urlsCard
 // const listsCardData = featuredCard.listsCard
-
