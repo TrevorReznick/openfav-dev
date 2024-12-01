2af41b1dc1dc178f5d4cd9abdda39834bff23324
Vincent Maximo II <projects.manager@xdomain.it>
2024-12-01 18:43:29 +0100

    OPF-59 testing query

--- src/pages/api/v1/main/events.ts
+++ src/pages/api/v1/main/events.ts
@@ -4,24 +4,14 @@ import { supabase } from "~/providers/supabase";
 export const GET: APIRoute = async () => {
   const { data, error } = await supabase.from("event_log").select(
     `
-      id,
-      id_event_family,
-      id_event_type,
-      user_id,
-      event_data,
-      event_family (
-        id,
-        event_family
-      ),
-      event_type (
-        id,
+      id,      
+      event_type (        
         event_type, 
         event_description
       ),
-      auth_users (
-        email
+      event_family (
+        event_family
       )
-
     `
   );
 
