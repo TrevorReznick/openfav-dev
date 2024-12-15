e31dd106a0d99317e2f1d73f434e35bd1bc67d3b
Trevor Reznick <electrics.sheeps@gmail.com>
2024-12-15 12:31:04 +0100

    OPF-71 refactoring

--- src/pages/api/v1/main/doQueries.ts
+++ src/pages/api/v1/main/doQueries.ts
@@ -43,84 +43,65 @@ const handleRequest = async (method: string, url: URL, request?: Request) => {
 const handleApiRequest = async (method: string, type: string, params: any, request?: Request) => {
   
   switch (type) {
-    case 'getSubCategories':
-      if (method !== 'GET') throw new Error('Invalid method for get event')
-        const getSubCategoriesData = request ? await request.json() : {}
-        return getSubCategories()
-    case 'getCategories':
-      if (method !== 'GET') throw new Error('Invalid method for get event')
-        const getCategoriesData = request ? await request.json() : {}
-        return getCategories()
+
+    /* @@ -- GET methods -- @@ */
+
     case 'getAreas':
-      if (method !== 'GET') throw new Error('Invalid method for get event')
-        const getAreasData = request ? await request.json() : {}
-        return getAreas()
-    case 'getTags':
-      if (method !== 'GET') throw new Error('Invalid method for get event')
-        const getSitesTagsData = request ? await request.json() : {}
-        return getTags()  
+      if (method !== 'GET') throw new Error('Invalid method for getAreas')
+      const getAreasData = request ? await request.json() : {}
+      return getAreas()
+  
+    case 'getCategories':
+      if (method !== 'GET') throw new Error('Invalid method for getCategories')
+      const getCategoriesData = request ? await request.json() : {}
+      return getCategories()
+  
+    case 'getCategoriesJson':
+      if (method !== 'GET') throw new Error('Invalid method for getCategoriesJson')
+      const getAllCategoriesData = request ? await request.json() : {}
+      return getCategoriesJson()
+  
     case 'getSites':
-      if (method !== 'GET') throw new Error('Invalid method for get event')
-        const insertSitesData = request ? await request.json() : {}
-        return getSites()//getSiteById
-     case 'getSiteById': {
+      if (method !== 'GET') throw new Error('Invalid method for getSites')
+      const getSitesData = request ? await request.json() : {}
+      return getSites()
+  
+    case 'getSiteById':
       if (method !== 'GET') throw new Error('Invalid method for getSiteById')
-        const { id } = params; // Ottieni l'id dai parametri
-        if (!id) throw new Error('ID is required for getSiteById')
-        return getSite(id); // Chiama la funzione getSite con l'id
-      }
-    
+      const { id } = params
+      if (!id) throw new Error('ID is required for getSiteById')
+      return getSite(id)
+  
+    case 'getSubCategories':
+      if (method !== 'GET') throw new Error('Invalid method for getSubCategories')
+      const getSubCategoriesData = request ? await request.json() : {}
+      return getSubCategories()
+  
+    case 'getTags':
+      if (method !== 'GET') throw new Error('Invalid method for getTags')
+      const getSitesTagsData = request ? await request.json() : {}
+      return getTags()
+  
+    /* @@ -- POST methods -- @@ */
+
     case 'insertEvent':
       if (method !== 'POST') throw new Error('Invalid method for insertEvent')
-        const insertData = request ? await request.json() : {}
-        return insertEvent(insertData)
+      const insertData = request ? await request.json() : {}
+      return insertEvent(insertData)
+  
+    /* @@ -- PUT methods -- @@ */
+
     case 'updateEvent':
       if (method !== 'PUT') throw new Error('Invalid method for updateEvent')
-        const updateData = request ? await request.json() : {}
-        return updateEvent(updateData, updateData.id)
+      const updateData = request ? await request.json() : {}
+      return updateEvent(updateData, updateData.id)
+  
     default:
-        throw new Error('Unknown API request type')
-    }
-}
-
-const insertEvent = async (data: any) => {
-    const tableName = 'event_log'
-    const result = await supabaseInsert(tableName, data)
-    if (!result.success) {
-        throw new Error(result.error)
-    }
-    return result.data
-}
-
-const updateEvent = async (data: any, id: string) => {
-
-  const tableName = 'event_log'; // Assumiamo che la tabella per gli eventi si chiami 'event_log'
-
-  const result = await supabaseUpdate(tableName, data, (query) => query.eq('id', parseInt(id)))
-
-  if (!result.success) {
-    throw new Error(result.error);
+      throw new Error('Unknown API request type')
   }
-
-  return result.data;
 }
 
-const getTags = async () => {
-  return await supabaseQuery('categories_tags', {
-    select: `
-      id_src,
-      id_area,      
-      id_cat,
-      tag_3,
-      tag_4,
-      tag_5 
-    `,
-    order: {
-      column: 'id_src', // Colonna da ordinare
-      ascending: true    // Ordine ascendente (true) o discendente (false)
-    }
-  })
-}
+/* @@ -- GET methods -- @@ */
 
 const getAreas = async () => {
   return await supabaseQuery('areas', {
@@ -142,23 +123,45 @@ const getCategories = async () => {
       category
     `,
     order: {
-      column: 'id', // Colonna da ordinare
-      ascending: true    // Ordine ascendente (true) o discendente (false)
+      column: 'id',
+      ascending: true
     }
   })
 }
 
-const getSubCategories = async () => {
+const getCategoriesJson = async () => {
   return await supabaseQuery('sub_categories', {
     select: `
       id,
+      sub_category,
       id_category,
-      sub_category
+      categories (
+        category,
+        id_area (
+          area
+        )
+      )
     `,
     order: {
-      column: 'id', // Colonna da ordinare
-      ascending: true    // Ordine ascendente (true) o discendente (false)
+      column: 'id',
+      ascending: true
     }
+  })
+}
+
+const getSite = async (id: string | number) => {
+  return await supabaseQuery('main_table', {
+    select: `
+      id,
+      description,
+      icon,
+      image,
+      logo,
+      name,
+      title,
+      url
+    `,
+    filter: (query) => query.eq('id', id)
   })
 }
 
@@ -199,21 +202,57 @@ const getSites = async () => {
     `
   })
 }
-const getSite = async (id: string | number) => {
-  return await supabaseQuery('main_table', {
+
+const getSubCategories = async () => {
+  return await supabaseQuery('sub_categories', {
     select: `
       id,
-      description,
-      icon,
-      image,
-      logo,
-      name,
-      title,
-      url
-      )
+      id_category,
+      sub_category
     `,
-    filter: (query) => query.eq('id', id)
+    order: {
+      column: 'id',
+      ascending: true
+    }
   })
+}
+
+const getTags = async () => {
+  return await supabaseQuery('categories_tags', {
+    select: `
+      id_src,
+      id_area,      
+      id_cat,
+      tag_3,
+      tag_4,
+      tag_5 
+    `,
+    order: {
+      column: 'id_src',
+      ascending: true
+    }
+  })
+}
+
+/* @@ -- POST methods -- @@ */
+const insertEvent = async (data: any) => {
+  const tableName = 'event_log'
+  const result = await supabaseInsert(tableName, data)
+  if (!result.success) {
+    throw new Error(result.error)
+  }
+  return result.data
+}
+
+/* @@ -- PUT methods -- @@ */
+
+const updateEvent = async (data: any, id: string) => {
+  const tableName = 'event_log'
+  const result = await supabaseUpdate(tableName, data, (query) => query.eq('id', parseInt(id)))
+  if (!result.success) {
+    throw new Error(result.error)
+  }
+  return result.data
 }
 
 
