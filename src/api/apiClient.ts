3e2fc78574b171e90c113cd71424f915f10ae64d
Trevor Reznick <electrics.sheeps@gmail.com>
2024-12-15 18:17:53 +0100

    OPF-71 adding del method

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -20,6 +20,8 @@ export const sendApiRequest = async (fetchFunc) => {
 
 const my_id = 49
 
+const ids = [66, 65 , 64]
+
 const newEvent = {
     id_event_type: 1,
     id_event_family: 2
@@ -31,32 +33,7 @@ const updatedEventData = {
     id_event_family: 2
 }
 
-const createPostEvent = async () => {
-    return makeRequest(api_endpoint, 'insertEvent', {}, 'POST', newEvent)
-}
-
-const createUpdateEvent = async () => {
-    return makeRequest(api_endpoint, 'updateEvent', {id: updatedEventData.id }, 'PUT', updatedEventData)
-}
-
-const fetchEvents = () => 
-    makeRequest(api_endpoint, 'getEvents') //TODO
-  
-const fetchSites = () => 
-    makeRequest(api_endpoint, 'getSites')
-
-const fetchSiteById = (id: number) =>
-    makeRequest(api_endpoint, 'getSiteById', { id: my_id })
-
-const fetchCategories = () => 
-    makeRequest(api_endpoint, 'getCategories')
-
-
-const fetchSubCategories = () => 
-    makeRequest(api_endpoint, 'getSubCategories')
-
-const fetchTags = () => 
-    makeRequest(api_endpoint, 'getTags')
+/* @@ -- GET methods -- @@ */
 
 const fetchAreas = () => 
     makeRequest(api_endpoint, 'getAreas')
@@ -64,27 +41,63 @@ const fetchAreas = () =>
 const fetchAreasCategoriesSubCategories = () => 
     makeRequest(api_endpoint, 'getCategoriesJson')
 
-/* @@ get valuse using api system 0.6.0 @@ */
+const fetchCategories = () => 
+    makeRequest(api_endpoint, 'getCategories')
 
-//export const insertEvent = await sendApiRequest(createPostEvent)
-//export const updateEvent = await sendApiRequest(createUpdateEvent)
-//export const getSites = await sendApiRequest(fetchSites)  
-  
-/* @@ exports the results @@ */
-   
-//export const events = await sendApiRequest(fetchEvents) //TODO
-export const sites = await sendApiRequest(fetchSites)
-export const tags = await sendApiRequest(fetchTags)
-export const areas = await sendApiRequest(fetchAreas)
-export const categories = await sendApiRequest(fetchCategories)
-export const sub_categories = await sendApiRequest(fetchSubCategories)//FIXME
-export const site = await sendApiRequest(fetchSiteById)
-export const all_categories = await sendApiRequest(fetchAreasCategoriesSubCategories)
-  
+const fetchEvents = () => 
+    makeRequest(api_endpoint, 'getEvents') //TODO
 
-/* @@ old @@ */
+const fetchSites = () => 
+    makeRequest(api_endpoint, 'getSites')
 
-const createUpdateEventOld = async () => {
+const fetchSiteById = (id: number) =>
+    makeRequest(api_endpoint, 'getSiteById', { id: my_id })
+
+const fetchSubCategories = () => 
+    makeRequest(api_endpoint, 'getSubCategories')
+
+const fetchTags = () => 
+    makeRequest(api_endpoint, 'getTags')
+
+/* @@ -- POST methods -- @@ */
+
+const createPostEvent = async () => {
     return makeRequest(api_endpoint, 'insertEvent', {}, 'POST', newEvent)
 }
+
+/* @@ -- PUT methods -- @@ */
+
+const createUpdateEvent = async () => {
+    return makeRequest(api_endpoint, 'updateEvent', {id: updatedEventData.id }, 'PUT', updatedEventData)
+}
+
+/* @@ -- DEL methods -- @@ */
+
+const delEvent = async (id: number) => {
+    return makeRequest(api_endpoint, 'deleteEvent', {id: id }, 'DELETE', {})
+}
+  
+   
+//export const events = await sendApiRequest(fetchEvents) //TODO
+
+/* @@ -- GET methods -- @@ */
+export const areas = await sendApiRequest(fetchAreas)
+export const categories = await sendApiRequest(fetchCategories)
+export const getSites = await sendApiRequest(fetchSites)
+export const sites = await sendApiRequest(fetchSites)
+export const site = await sendApiRequest(fetchSiteById)
+export const sub_categories = await sendApiRequest(fetchSubCategories) //FIXME
+export const tags = await sendApiRequest(fetchTags)
+export const all_categories = await sendApiRequest(fetchAreasCategoriesSubCategories)
+
+/* @@ -- POST methods -- @@ */
+export const insertEvent = await sendApiRequest(createPostEvent)
+
+/* @@ -- PUT methods -- @@ */
+export const updateEvent = await sendApiRequest(createUpdateEvent)
+
+/* @@ -- DEL methods -- @@ */
+export const deleteEvent = await sendApiRequest(delEvent)
+  
+
 
