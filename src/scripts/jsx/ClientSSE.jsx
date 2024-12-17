7e1cf87edd2a468c9c69ccd5782ff4da51d5ac4d
TrevorReznick <electrics.sheeps@gmail.com>
2024-12-17 10:21:40 +0100

    OPF-73 new client component

--- src/scripts/jsx/ClientSSE.jsx
+++ src/scripts/jsx/ClientSSE.jsx
@@ -1,13 +1,16 @@
 // ~/components/SSEClient.jsx
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react';
 
-const SSEClient = () => {
+const SSEClient = ({ url }) => {
+  const [lastMessage, setLastMessage] = useState('');
+
   useEffect(() => {
-    const eventSource = new EventSource('https://openfav-dev.vercel.app/api/v1/sse');
+    const eventSource = new EventSource(url);
 
     eventSource.onmessage = (event) => {
       const message = event.data;
       console.log('Received message:', message);
+      setLastMessage(message);
 
       // Controlla se il messaggio inizia con "ALERT: "
       if (message.startsWith('ALERT: ')) {
@@ -23,9 +26,14 @@ const SSEClient = () => {
     return () => {
       eventSource.close();
     };
-  }, []);
+  }, [url]);
 
-  return <div>SSE Client</div>;
-}
+  return (
+    <div>
+      <h2>SSE Client</h2>
+      <p>Last Message: {lastMessage}</p>
+    </div>
+  );
+};
 
-export default SSEClient
+export default SSEClient;
