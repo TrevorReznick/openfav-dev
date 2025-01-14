27d9824d5bb0d6867fc45222c5f97ff5fe704fb5
TrevorReznick <electrics.sheeps@gmail.com>
2025-01-14 09:21:54 +0100

    OPF-55 auth navbar

---
+++ src/config/navbar.ts
@@ -0,0 +1,15 @@
+export const navbarLinks = [
+    { label: 'Home', href: '/', ariaLabel: 'Home' },
+    { label: 'Features', href: '/#whats-new', ariaLabel: 'Features' },
+    { label: 'How it works', href: '/#how-it-works', ariaLabel: 'Pricing' },
+    { label: 'About Us', href: '/about', ariaLabel: 'About' },
+    { label: 'Contacts', href: '/dev/feedback', ariaLabel: 'FAQ' },
+]
+
+export const navbarAuthLinks = [
+    { label: 'Home', href: '/', ariaLabel: 'Home' },
+    { label: 'Dashboard', href: '/protected/dashboard', ariaLabel: 'Features' },
+    { label: 'Links Showcase', href: '/dev/sites-list', ariaLabel: 'Pricing' },
+    { label: 'My Lists', href: '/about', ariaLabel: 'About' },
+    { label: 'Contacts', href: '/dev/feedback', ariaLabel: 'FAQ' },
+]
